ae8e2036c35f631ef323ddfe3305156433cc42a8
Rusiru <rdha868@aucklanduni.ac.nz>
2023-05-28 15:29:36 +1200

    passed nearly all task 1 tests still confused

--- src/main/java/nz/ac/auckland/se281/datastructures/Graph.java
+++ src/main/java/nz/ac/auckland/se281/datastructures/Graph.java
@@ -22,25 +22,39 @@ public class Graph<T extends Comparable<T>> {
     this.verticies=verticies;
     this.edges=edges;
   }
-  
+  public int getInDegree(T vertex) {
+    int InDegree=0;
+    
+    for(Edge<T> edge :edges){
+      if(edge.returnDestination().equals(vertex)){InDegree++;
+      }
+    }
+    return InDegree;
+  }
+
   public Set<T> getRoots() {
     // TODO: Task 1.
    
      Set<T> x=new HashSet<>();
-     
-    for (Edge<T> edge : edges) {
-      int y=0;
-      for (Edge<T> z : edges) {
+    
+    
+        for(T verticie :verticies){
+          int y=0;
+           y=getInDegree(verticie);
 
-        if (!edge.returnSource().equals(z.returnDestination())) {
-          y++;
-           
-        }}
-        if(y==edges.size()){x.add(edge.returnSource());
+            
+          
+          if(y==0){
+            x.add(verticie);
+          }
+
         }
+      
+       
 
 
-        }
+        
+      
         return x;
       }
      
@@ -50,33 +64,136 @@ public class Graph<T extends Comparable<T>> {
    
 
   public boolean isReflexive() {
-    // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    int x=0;
+    for(Edge<T> edge :edges){
+      if(edge.returnDestination().equals(edge.returnSource())){
+        x++;
+    }}
+    if(x==verticies.size()){return true;}
+    else{return false;}
   }
 
   public boolean isSymmetric() {
     // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    int x=0;
+    for(Edge<T> edge :edges){
+      
+      for (Edge<T> edge1 :edges){
+        if( edge.returnDestination().equals(edge1.returnSource())&&edge.returnSource().equals(edge1.returnDestination())){
+         /* System.out.println(edge.returnSource());
+          System.out.println(edge.returnDestination());
+          System.out.println("x");
+          System.out.println(edge1.returnSource());
+          System.out.println(edge1.returnDestination());
+          System.out.println("z");*/
+          x++;
+        }
+
+      }}
+
+      
+      if(x==edges.size()){
+        return true;
+    }
+  else{return false;}
+
+    
+
   }
 
   public boolean isTransitive() {
-    // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    int x =0;
+    int y=0;
+    for(Edge<T> edge :edges){
+      for (Edge<T> edge1 :edges){
+       
+        if(edge.returnDestination().equals(edge1.returnSource())&&!edge1.returnDestination().equals(edge1.returnSource())){
+          x++;
+        
+          
+            
+            
+            for(Edge<T> edge3:edges){
+              if(edge.returnSource().equals(edge3.returnSource())&&edge1.returnDestination().equals(edge3.returnDestination())){y++;}
+              else{}
+              
+            }
+
+          
+        
+          }
+        }
+        
+      }
+      if(x==y){
+        return true;}
+        else{
+          return false;
+        }
   }
 
   public boolean isAntiSymmetric() {
-    // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    int x=0;
+    int y=0;
+    for(Edge<T> edge :edges){
+
+      for (Edge<T> edge1 :edges){
+       
+        if( edge.returnDestination().equals(edge1.returnSource())&&edge.returnSource().equals(edge1.returnDestination())){
+y++;
+          if( edge.equals(edge1)){
+            x++;
+
+           
+          }
+
+          if(!edge.returnDestination().equals(edge1.returnSource())&&edge.returnSource().equals(edge1.returnDestination())){}
+          
+        }
+        else{
+}
+
+      }}
+
+        
+      if(y==x){
+        return true;
+    }
+  else{
+    
+    return false;}
+
+    
   }
 
   public boolean isEquivalence() {
-    // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    if(isReflexive() && isSymmetric() && isTransitive() ){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 
   public Set<T> getEquivalenceClass(T vertex) {
+    Set<T> x=new HashSet<>();
+    if (!isEquivalence()) {
+      return x;
+    }
+    else{
+      for(Edge<T> edge :edges){
+        if(edge.returnSource().equals(vertex)){
+          x.add(edge.returnDestination());
+        }
+        else if(edge.returnDestination().equals(vertex)){
+          x.add(edge.returnSource());
+        }
+        else{}
+      }
+      return x;
+    }
     // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    
   }
 
   public List<T> iterativeBreadthFirstSearch() {
